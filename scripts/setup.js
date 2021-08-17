458533f370d3820d8b79ac1fa2512544010cf8d7
gurleenbindra <67560143+gurleenbindra@users.noreply.github.com>
2021-08-17 18:12:46 +0530

    Update setup.js

--- scripts/setup.js
+++ scripts/setup.js
@@ -5,6 +5,7 @@ const data = {
   GITHUB_RUN_NUMBER: process.env.GITHUB_RUN_NUMBER,
   GITHUB_REPOSITORY: process.env.GITHUB_REPOSITORY,
   GITHUB_REF: process.env.GITHUB_REF,
+  API_PORT: process.env.PORT || 3000
 };
 
 fs.writeFileSync(process.argv[2], JSON.stringify(data));
